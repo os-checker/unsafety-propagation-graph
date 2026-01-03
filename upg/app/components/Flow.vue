dc2c0a2db07b7e1bd59a8ad1fe65299d83293c2e
zjp <jiping_zhou@foxmail.com>
2026-01-03 09:08:10 +0000

    ui: recompute layout by dagre

--- upg/app/components/Flow.vue
+++ upg/app/components/Flow.vue
@@ -1,13 +1,13 @@
 <template>
-  <VueFlow :nodes="data.nodes" :edges="data.edges"></VueFlow>
+  <VueFlow :nodes="data.nodes" :edges="data.edges" @nodes-initialized="layoutGraph('LR')"></VueFlow>
 </template>
 
 <script setup lang="ts">
 import type { Node, Edge } from '@vue-flow/core'
 import { VueFlow, useVueFlow } from '@vue-flow/core'
 
-// const { addNodes, $reset, getNodes } = useVueFlow();
-
+const { fitView } = useVueFlow();
+const { layout } = useLayout();
 const url = "https://raw.githubusercontent.com/os-checker/unsafety-propogation-graph-data/refs/heads/main/test/demo/function/S%3A%3Awrite_field.json"
 
 type Function = {
@@ -29,14 +29,21 @@ $fetch(url)
   .then(text => raw.value = JSON.parse(text as string))
   .catch(err => console.log(err));
 
-const data = computed<{ nodes: Node[], edges: Edge[] }>(() => {
-  const val = raw.value;
-  if (!val.name) return { nodes: [], edges: [] };
+type Data = { nodes: Node[], edges: Edge[] };
+const EMPTY_DATA = { nodes: [], edges: [] };
+
+const data = ref<Data>(EMPTY_DATA);
+
+watch(raw, val => {
+  if (!val.name) return;
+
+  // Placeholder for initial position. The layout will be recomputed later.
+  const POS = { x: 0, y: 0 };
 
   // Add the current function as root node, callees and adts as leaves.
-  const root: Node = { id: val.name, position: { x: 200, y: 100 }, data: { label: val.name } };
-  const callees: Node[] = val.callees.map((callee, idx) => ({ id: `c@${callee}`, type: "input", position: { x: 200 * idx, y: 0 }, label: callee }));
-  const adts: Node[] = Object.keys(val.adts).map((adt, idx) => ({ id: `a@${adt}`, type: "output", position: { x: 200 * idx, y: 200 }, label: adt }));
+  const root: Node = { id: val.name, label: val.name, position: POS };
+  const callees: Node[] = val.callees.map(callee => ({ id: `c@${callee}`, type: "input", label: callee, position: POS }));
+  const adts: Node[] = Object.keys(val.adts).map(adt => ({ id: `a@${adt}`, type: "output", label: adt, position: POS }));
   const nodes = [root, ...callees, ...adts];
 
   let edges: Edge[] = [];
@@ -44,7 +51,14 @@ const data = computed<{ nodes: Node[], edges: Edge[] }>(() => {
   callees.forEach(leaf => edges.push({ id: `e@${root.id}-${leaf.id}`, source: leaf.id, target: root.id, }));
   adts.forEach(leaf => edges.push({ id: `e@${root.id}-${leaf.id}`, source: root.id, target: leaf.id, }));
 
-  return { nodes, edges }
+  data.value = { nodes, edges };
 })
+
+/** Recompute node layout (position). */
+async function layoutGraph(direction: string) {
+  if (data.value.nodes.length === 0) return;
+  data.value.nodes = layout(data.value.nodes, data.value.edges, direction)
+  nextTick(fitView);
+}
 
 </script>
