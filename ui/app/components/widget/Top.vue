890cfdb021fe6b73bf40766abfd8a34ff1380e73
zjp <jiping_zhou@foxmail.com>
2026-01-07 10:03:07 +0000

    ui: add View Type selections

--- ui/app/components/widget/Top.vue
+++ ui/app/components/widget/Top.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="top-menu">
-    <UNavigationMenu :items="items" />
+    <UNavigationMenu :items="menuItems" />
+
+    <div>
+      Graph View:
+      <USelectMenu v-model="viewSelected" multiple :items="views" :search-input="false" class="w-50" />
+    </div>
 
     <div class="top-menu mr-2">
       <UColorModeButton />
@@ -12,7 +17,15 @@
 <script setup lang="ts">
 import type { NavigationMenuItem } from '@nuxt/ui';
 
-const items = ref<NavigationMenuItem[]>([{ label: "function", }]);
+const menuItems = ref<NavigationMenuItem[]>([{ label: "function", }]);
+
+enum ViewType {
+  Callees = "Callees",
+  Adts = "Adts",
+}
+
+const viewSelected = ref<ViewType[]>([ViewType.Callees, ViewType.Adts]);
+const views = ref<ViewType[]>([ViewType.Callees, ViewType.Adts]);
 </script>
 
 <style lang="css">
