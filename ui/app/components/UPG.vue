ff8a939bc57cb94af53073d5760f78431a850d9b
zjp <jiping_zhou@foxmail.com>
2026-01-07 08:57:42 +0000

    ui: clean dead code

--- ui/app/components/UPG.vue
+++ ui/app/components/UPG.vue
@@ -13,24 +13,9 @@
 </template>
 
 <script setup lang="ts">
-import { useWindowSize } from "@vueuse/core"
 import type { Function } from "~/lib/output"
 import { EMPTY_FUNCTION } from "~/lib/output"
 import { Panel } from "~/lib/panel"
-
-const { width, height } = useWindowSize();
-const flowWidthRatio = 0.9;
-const flowHeightRatio = 0.6;
-const flowStyle = computed(() => ({
-  width: `${width.value * flowWidthRatio}px`,
-  height: `${height.value * flowHeightRatio}px`,
-}));
-const codeStyle = computed(() => ({
-  width: `${width.value * 0.98}px`,
-  height: `${height.value * (1 - flowHeightRatio) * 0.9}px`,
-  display: "grid",
-  gridTemplateColumns: "1fr 1fr",
-}));
 
 const url = "https://raw.githubusercontent.com/os-checker/unsafety-propagation-graph-data/refs/heads/main/test/poc/function/f.json"
 
