b7da45be5e51b8bac68bd2274ad41500be1dfee8
zjp <jiping_zhou@foxmail.com>
2026-01-10 17:37:04 +0000

    ui: naviItemClick callback

--- ui/app/components/widget/TopBar.vue
+++ ui/app/components/widget/TopBar.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="top-menu">
     <UNavigationMenu :items="navi_menu" class="w-1/2" trailing-icon="tabler:chevron-right">
-      <template #item-content="{ index }">
+      <template #item-content="{ index: stack_idx }">
         <div class="flex gap-4 m-2">
-          <div v-for="[kind, v_idx] in Object.entries(navi.navi[0]?.groups ?? {})" :key="kind">
+          <div v-for="[kind, v_sub_navi_idx] in Object.entries(navi.navi[0]?.groups ?? {})" :key="kind">
             <div :class="[colorClass(kind), 'text-center font-bold']">{{ kind }}</div>
             <div>
-              <ul>
-                <li v-for="item in v_idx.map(idx => navi.navi[0]?.subitems[idx]?.name ?? 'Unknown')" class="my-1">
-                  <UButton :label="item" :icon="icon(kind)" size="md" color="neutral" variant="ghost" />
-
+              <ul @click="(event) => naviItemClick(event, stack_idx)">
+                <li v-for="{ sub_navi_idx, item } in v_sub_navi_idx.map(
+                  idx => ({ sub_navi_idx: idx, item: navi.navi[0]?.subitems[idx] })
+                )" :data-idx="item?.idx" :data-sub-navi-idx="sub_navi_idx" class="my-1">
+                  <UButton :label="item?.name ?? 'ERROR-NAME'" :icon="icon(kind)" size="md" color="neutral"
+                    variant="ghost" />
                 </li>
               </ul>
             </div>
@@ -42,25 +44,43 @@ $fetch(NAVI_URL)
   .then(text => navi.value = JSON.parse(text as string))
   .catch(err => console.log(err));
 
-const navi_menu = computed<NavigationMenuItem[]>(() => {
-  const data = navi.value.data;
-  const nav = navi.value.navi;
+const navi_menu = ref<NavigationMenuItem[]>([]);
+const navi_stack = ref<number[]>([]);
+watch(navi, val => {
+  const data = val.data;
+  const nav = val.navi;
   const root = data[0]?.[0];
-  if (!root) return [];
+  if (!root) {
+    navi_menu.value = [];
+    navi_stack.value = [];
+    return;
+  }
 
-  let children = nav[0]?.subitems!.map(item => ({ label: item.name, icon: icon(item.kind) }));
-  let tree: NavigationMenuItem[] = [
-    {
-      label: root.name, icon: icon(root.kind), children
-    },
-    // ...children
-  ];
-
-  // nav[0]!.map(item => tree.push({ label: item.name }))
-
-  console.log(tree)
-  return tree
+  const tree: NavigationMenuItem[] = [{
+    label: root.name, icon: icon(root.kind),
+    children: nav[0]?.subitems!.map(item => ({ label: item.name, icon: icon(item.kind) }))
+  }];
+  navi_menu.value = tree;
 });
 
-
+/** Respond to which navi item is clicked.
+* stack_idx refers to the current position in navi_stack.
+* li id refers to navi data idx.
+*/
+function naviItemClick(event: MouseEvent, stack_idx: number) {
+  const li = (event.target as HTMLElement).closest('li')
+  if (li && (event.currentTarget as HTMLElement).contains(li)) {
+    const idx = parseInt(li.dataset.idx ?? "");
+    const sub_navi_idx = parseInt(li.dataset.subNaviIdx ?? "");
+    console.log(li.dataset.subNaviIdx)
+    // This is never null, because we just clicked it.
+    const clicked = {
+      full_path: navi.value.data[idx]!,
+      kind: navi.value.navi[stack_idx]?.subitems[sub_navi_idx]!
+    };
+    // This can be null when fn item is clicked.
+    const target = navi.value.navi[idx]?.subitems;
+    console.log("\nstack_idx:", stack_idx, "\nsub_navi_idx:", sub_navi_idx, "\nclicked:", clicked, "\ntarget:", target);
+  }
+}
 </script>
