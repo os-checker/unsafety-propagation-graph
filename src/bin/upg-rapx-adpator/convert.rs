92d7670bf046195177370bb652e412e966223b53
zjp <jiping_zhou@foxmail.com>
2026-01-17 08:05:45 +0000

    upg-rapx-adpator: print fn names that are not in mapping
    std::thread::spawn_unchecked is not in mapping
    core::io::borrowed_buf::advance_unchecked is not in mapping
    core::intrinsics::drop_in_place is not in mapping
    core::intrinsics::pref_align_of is not in mapping
    std::raw_attribute is not in mapping
    std::collections::hash::map::get_many_unchecked_mut is not in mapping
    core::ptr::mut_ptr::sub_ptr is not in mapping
    core::ptr::const_ptr::non_null::sub_ptr is not in mapping
    core::ptr::non_null::sub_ptr is not in mapping
    core::mem::maybe_uninit::slice_assume_init_mut is not in mapping
    core::mem::maybe_uninit::slice_assume_init_ref is not in mapping
    core::ptr::offset is not in mapping
    core::any::downcast_mut_unchecked is not in mapping
    core::ptr::const_ptr::sub_ptr is not in mapping
    std::sys::pal::unix::process::process_common::pre_exec is not in mapping
    std::thread::from_raw is not in mapping
    std::from_raw_handle is not in mapping
    core::io::borrowed_buf::set_init is not in mapping
    core::intrinsics::min_align_of_val is not in mapping
    std::sys::pal::unix::fs::from_raw_fd is not in mapping
    std::sys::anonymous_pipe::unix::from_raw_fd is not in mapping
    std::sys::pal::unix::net::from_raw_fd is not in mapping
    core::intrinsics::copy::copy is not in mapping
    core::any::downcast_ref_unchecked is not in mapping
    std::sys::pal::unix::fd::from_raw_fd is not in mapping
    alloc::boxed::downcast_unchecked is not in mapping

--- src/bin/upg-rapx-adpator/convert.rs
+++ src/bin/upg-rapx-adpator/convert.rs
@@ -3,28 +3,47 @@ use serde::Deserialize;
 use std::{collections::HashMap, env, fs};
 
 pub fn run() {
+    // Read std.json
     let file = env::var("RAPX_STD_JSON").unwrap();
     let file = fs::File::open(file).unwrap();
-    let std_json: HashMap<String, Data> = serde_json::from_reader(file).unwrap();
+    let std_json: HashMap<String, InputData> = serde_json::from_reader(file).unwrap();
     dbg!(std_json.len());
 
+    // Read mapping JSONs.
     let dir = env::var("RAPX_STD_MAPPING_DIR").unwrap();
-    let mut mapping = Vec::<FunctionName>::with_capacity(1024);
+    let mut mapping_json = Vec::<FunctionName>::with_capacity(1024);
     for entry in fs::read_dir(dir).unwrap() {
         let entry = entry.unwrap();
         let path = entry.path();
         if path.extension().and_then(|s| s.to_str()) == Some("json") {
-            let mapping_json = fs::File::open(path).unwrap();
-            let mapping_json: Vec<FunctionName> = serde_json::from_reader(mapping_json).unwrap();
-            mapping.extend(mapping_json);
+            let json = fs::File::open(path).unwrap();
+            let json: Vec<FunctionName> = serde_json::from_reader(json).unwrap();
+            mapping_json.extend(json);
+        }
+    }
+    let len = dbg!(mapping_json.len());
+
+    // Merge mapping JSONs by rapx key.
+    let mut mapping = HashMap::<String, Vec<FunctionName>>::with_capacity(len);
+    for item in mapping_json {
+        if let Some(v) = mapping.get_mut(&item.rapx) {
+            v.push(item);
+        } else {
+            mapping.insert(item.rapx.clone(), vec![item]);
         }
     }
     dbg!(mapping.len());
+
+    for (raw_fn_name, tags) in &std_json {
+        if !mapping.contains_key(raw_fn_name) {
+            eprintln!("{raw_fn_name} is not in mapping");
+        }
+    }
 }
 
 // "core::alloc::global::GlobalAlloc::alloc": { "0": [ "ValidNum", "Init" ] },
 #[derive(Debug, Deserialize)]
-struct Data {
+struct InputData {
     #[serde(rename = "0")]
     tags: Vec<String>,
 }
