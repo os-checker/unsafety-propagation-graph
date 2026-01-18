5ef1d5caaaf42e8c1584535fb6282bec87f122cd
zjp <jiping_zhou@foxmail.com>
2026-01-18 06:48:30 +0000

    run.sh: separate scripts into functions

--- run.sh
+++ run.sh
@@ -18,32 +18,44 @@ export RAPX_STD_MAPPING_DIR=$PWD/assets/fn_name_mapping
 export RAPX_STD_SPEC=$PWD/assets/specs/sp-core.toml
 export RAPX_STD_OUT=$PWD/data/tags/std.json
 
-pushd tests/demo
-cargo clean
-# Generate $UPG_DIR/$crate
-UPG_DIR=$UPG_DIR/demo upg
+gen_std() {
+  pushd tests/demo
+  cargo clean
+  # Generate $UPG_DIR/$crate
+  UPG_DIR=$UPG_DIR/demo upg
+  popd
+}
 
-# Generate
-# UPG_DRIVER=upg-rapx-adpator upg
-# cp "$UPG_DIR"/_rapx/core.json assets/fn_name_mapping/
-# cp "$UPG_DIR"/_rapx/std.json assets/fn_name_mapping/
-# cp "$UPG_DIR"/_rapx/alloc.json assets/fn_name_mapping/
+gen_fn_name_mapping() {
+  pushd tests/demo
+  cargo clean
+  UPG_DRIVER=upg-rapx-adpator upg
+  cp "$UPG_DIR"/_rapx/core.json assets/fn_name_mapping/
+  cp "$UPG_DIR"/_rapx/std.json assets/fn_name_mapping/
+  cp "$UPG_DIR"/_rapx/alloc.json assets/fn_name_mapping/
+  popd
+}
 
-# Generate std.json to $RAPX_STD_OUT
-# export LD_LIBRARY_PATH=$(rustc --print sysroot)/lib
-# UPG_RAPX_CONVERT=1 SP_FILE="$RAPX_STD_SPEC" upg-rapx-adpator
-popd
+gen_std_json() {
+  # Generate std.json to $RAPX_STD_OUT
+  export LD_LIBRARY_PATH=$(rustc --print sysroot)/lib
+  UPG_RAPX_CONVERT=1 SP_FILE="$RAPX_STD_SPEC" upg-rapx-adpator
+}
 
-pushd ./tag-asterinas/ostd/
-cargo clean
-UPG_DIR=$UPG_DIR/asterinas CARGO_BUILD_ARGS="--target x86_64-unknown-none" upg
-popd
+gen_data() {
+  pushd ./tag-asterinas/ostd/
+  cargo clean
+  UPG_DIR=$UPG_DIR/asterinas CARGO_BUILD_ARGS="--target x86_64-unknown-none" upg
+  popd
 
-# Prune logs.
-rm target/upg/**/upg.log
-rm target/upg/**/rustflags.json
+  # Prune logs.
+  rm target/upg/**/upg.log
+  rm target/upg/**/rustflags.json
 
-# cp target/upg/asterinas/alloc data -r
-# cp target/upg/asterinas/core data -r
-# cp target/upg/asterinas/ostd data -r
-# cp target/upg/demo/std data -r
+  # cp target/upg/asterinas/alloc data -r
+  # cp target/upg/asterinas/core data -r
+  # cp target/upg/asterinas/ostd data -r
+  # cp target/upg/demo/std data -r
+}
+
+gen_std_json
