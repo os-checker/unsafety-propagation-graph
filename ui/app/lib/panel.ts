028d2828459b568f70154ff290984f751692b58d
zjp <jiping_zhou@foxmail.com>
2026-01-23 15:58:45 +0000

    feat(ui): add panel kinds in URL query

--- ui/app/lib/panel.ts
+++ ui/app/lib/panel.ts
@@ -11,3 +11,22 @@ export type PanelContent = {
   nodeItem: string
 }
 
+export function toPanelStr(p: Panel): string {
+  switch (p) {
+    case Panel.Src: return "src";
+    case Panel.Mir: return "mir";
+    case Panel.Doc: return "doc";
+    case Panel.Tag: return "tag";
+    default: return ""
+  }
+}
+
+export function toPanel(s: string): Panel | undefined {
+  if (!s) return undefined;
+  switch (s) {
+    case "src": return Panel.Src;
+    case "mir": return Panel.Mir;
+    case "doc": return Panel.Doc;
+    case "tag": return Panel.Tag;
+  }
+}
