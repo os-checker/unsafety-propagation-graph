f68e87ded9e05423fa815f55b720bce009d0471b
zjp <jiping_zhou@foxmail.com>
2026-01-26 12:09:25 +0000

    ui(nuxt-config): clean dead code

--- ui/nuxt.config.ts
+++ ui/nuxt.config.ts
@@ -34,41 +34,4 @@ export default defineNuxtConfig({
       ],
     }
   },
-  // vite: {
-  //   optimizeDeps: {
-  //     // 预构建这些库，防止它们在开发环境下导致页面多次刷新，
-  //     // 也能帮助 Vite 在生产环境下更稳定地处理它们。
-  //     include: ['elkjs', '@dagrejs/dagre', '@dagrejs/graphlib',]
-  //   },
-  //   build: {
-  //     // 1. 必须禁用，否则 elkjs 的巨大体积会导致这个补丁注入失败
-  //     modulePreload: { polyfill: false },
-  //
-  //     // 2. 适当调高警告阈值（治标）
-  //     chunkSizeWarningLimit: 3000,
-  //
-  //     commonjsOptions: {
-  //       // 允许处理 node_modules 里的 CommonJS 模块
-  //       include: [/@dagrejs\/dagre/, /@dagrejs\/graphlib/, /node_modules/],
-  //       // 关键：很多库会用 try-catch 包裹 require，设置此项为 false 能跳过检查
-  //       ignoreTryCatch: false
-  //     }
-  //   },
-  // },
-  // 2. 核心修复：针对 Nitro 服务端的配置
-  // nitro: {
-  //   externals: {
-  //     // 强制 Nitro 在服务端构建时将这些包内联
-  //     // 这样可以避免运行时出现 "Dynamic require" 错误
-  //     inline: [
-  //       '@dagrejs/dagre',
-  //       '@dagrejs/graphlib',
-  //       'elkjs'
-  //     ]
-  //   },
-  // },
-  // build: {
-  //   transpile: ['elkjs',]
-  // },
-  // devtools: { enabled: true }
 })
